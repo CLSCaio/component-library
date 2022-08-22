2cb750ca89de8042be8d1ffcf2f8d83c6309402a
Caio Sousa <caiodelimack@hotmail.com>
2022-08-22 18:50:52 -0300

    fix: removendo propriedade display para corrigir bug do component

--- src/components/content/description/styles.ts
+++ src/components/content/description/styles.ts
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components';
 import * as I from './interface';
 
 export const Text = styled.p<I.DescriptionStyles>`
-  display: flex;
   width: ${({ width }) => (width ? `${width}px` : 'max-content')};
 
   ${({ align }) =>
