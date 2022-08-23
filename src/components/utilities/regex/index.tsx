1f7572dd73aeb70fe2d12587727cbca7ab734b84
Caio Sousa <caiodelimack@hotmail.com>
2022-08-22 21:44:08 -0300

    feat: adicionando regex de telefone e celular

--- src/components/utilities/regex/index.tsx
+++ src/components/utilities/regex/index.tsx
@@ -1,17 +1,21 @@
 const strongPassword =
-  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#!%&'"])[0-9a-zA-Z$*&@#!%&'"]{8,}$/;
+  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#!%&'"])[0-9a-zA-Z$*&@#!%&'"]{8,}$/gm;
 
-const cpf = /([\d]{3}.){2}[\d]{3}-[\d]{2}/;
+const cpf = /^([\d]{3}.){2}[\d]{3}-[\d]{2}$/gm;
 
-const cnpj = /[\d]{2}.[\d]{3}.[\d]{3}\/[\d]{4}-[\d]{2}/;
+const cnpj = /^[\d]{2}.[\d]{3}.[\d]{3}\/[\d]{4}-[\d]{2}$/gm;
 
-const cep = /[\d]{5}-[\d]{3}/;
+const cep = /^[\d]{5}-[\d]{3}$/gm;
 
-const date = /([\d]{2}\/){2}[\d]{4}/;
+const date = /^([\d]{2}\/){2}[\d]{4}$/gm;
 
-const shortDate = /[\d]{2}\/[\d]{4}/;
+const shortDate = /^[\d]{2}\/[\d]{4}$/gm;
 
-const email = /^[a-z0-9.]+@[a-z]+\.((com)((.br)?))/;
+const email = /^[a-z0-9.]+@[a-z]+\.((com)((.br)?))$/gm;
+
+const cellphone = /^\([\d]{2}\) 9 [\d]{4}-[\d]{4}$/gm;
+
+const phone = /^\([\d]{2}\) [\d]{4}-[\d]{4}$/gm;
 
 export const regex = {
   strongPassword,
@@ -21,4 +25,6 @@ export const regex = {
   date,
   shortDate,
   email,
+  cellphone,
+  phone,
 };
